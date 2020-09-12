02dfd771384f883e1d5a9e853c45bb0b6f4d36cc
Adam Arthur Faizal <adam.faizal.af6@gmail.com>
2020-09-12 16:45:30 +0700

    Menyelesaikan latihan 1 & 2

---
+++ Pertemuan3/latihan1.php
@@ -0,0 +1,78 @@
+<?php
+// Variabel & tipe data
+$nama = "Kirigaya Kazuto";   // String
+$nama_panggilan = "Kirito";     // String
+$umur = 17;     // Integer
+$jenis_kelamin = 'L';   // Char
+$menikah = true;   // boolean
+$tinggi_badan = 164.5;  // float
+
+$nama2 = "Yuuki Asuna";
+$nama_panggilan2 = "Asuna";
+$umur2 = 18;
+$jenis_kelamin2 = 'P';
+$menikah2 = false;
+$tinggi_badan2 = 164.5;
+
+$nama3 = "Adam Arthur Faizal";
+$nama_panggilan3 = "Arthur";
+$umur3 = 18;
+$jenis_kelamin3 = 'L';
+$menikah3 = false;
+$tinggi_badan3 = 162;
+?>
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Latihan 1</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
+</head>
+<body>
+<div class="container mt-5">
+    <h1 class="text-center">Latihan 1</h1>
+    <table class="table">
+        <thead class="thead-dark">
+        <tr>
+            <th scope="col">No</th>
+            <th scope="col">Nama</th>
+            <th scope="col">Panggilan</th>
+            <th scope="col">Umur</th>
+            <th scope="col">Jenis Kelamin</th>
+            <th scope="col">Tinggi Badan</th>
+        </tr>
+        </thead>
+        <tbody>
+        <tr>
+            <th scope="row">1</th>
+            <td><?= $nama ?></td>
+            <td><?= $nama_panggilan ?></td>
+            <td><?= $umur ?> tahun</td>
+            <td><?= $jenis_kelamin ?></td>
+            <td><?= $tinggi_badan ?> cm</td>
+        </tr>
+        <tr>
+            <th scope="row">2</th>
+            <td><?= $nama2 ?></td>
+            <td><?= $nama_panggilan2 ?></td>
+            <td><?= $umur2 ?> tahun</td>
+            <td><?= $jenis_kelamin2 ?></td>
+            <td><?= $tinggi_badan2 ?> cm</td>
+        </tr>
+        <tr>
+            <th scope="row">3</th>
+            <td><?= $nama3 ?></td>
+            <td><?= $nama_panggilan3 ?></td>
+            <td><?= $umur3 ?> tahun</td>
+            <td><?= $jenis_kelamin3 ?></td>
+            <td><?= $tinggi_badan3 ?> cm</td>
+        </tr>
+        </tbody>
+    </table>
+</div>
+
+</body>
+</html>
---
+++ Pertemuan3/latihan2.php
@@ -0,0 +1,57 @@
+<?php
+
+//$nama = "Kirigaya Kazuto";   // String
+//$nama_panggilan = "Kirito";     // String
+//$umur = 17;     //
+//$jenis_kelamin = 'L';   // Char
+//$menikah = false;   // boolean
+//$tinggi_badan = 164.5;  // float
+//
+//$nama2 = "Yuuki Asuna";
+//$nama_panggilan2 = "Asuna";
+//$umur2 = 18;
+//$jenis_kelamin2 = 'P';
+//$menikah2 = false;
+//$tinggi_badan2 = 164.5;
+//
+//$nama3 = "Ken Kaneki";
+//$nama_panggilan3 = "Kaneki";
+//$umur3 = 17;
+
+$nama = "Adam";
+$nama_panggilan = "Arthur";
+
+
+// Mencoba IF - ELSE
+//if ($umur > 17) {
+//    echo "Lebih dari 17 th";
+//} elseif ($umur < 17) {
+//    echo "Kurang dari 17 th";
+//} else {
+//    echo "Sama dengan 17 th\n";
+//    if ($menikah === true) {
+//        echo "Sudah menikah";
+//    } else {
+//        echo "Belum menikah";
+//    }
+//}
+
+// Looping
+// For
+
+for ($i = 0; $i <= 10; $i++) {
+    echo "Halo, nama saya $nama biasa dipanggil $nama_panggilan <br>";
+}
+
+// while
+$nilai = 1;
+//while ($nilai < 10){
+//    echo "Hello, World!";
+//    $nilai++;
+//}
+
+// do - while
+//do {
+//    echo "Hello, World!";
+//} while ($nilai > 10);
+
